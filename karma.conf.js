824d8a1e2892a66f81b2d1fbe90d9765301b1e00
david mimay <davidmimay@gmail.com>
2022-11-24 19:20:57 +0000

    rename to avoid conflict

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular.io-example'),
+      dir: require('path').join(__dirname, './coverage/angularbase'),
       subdir: '.',
       reporters: [
         { type: 'html' },
